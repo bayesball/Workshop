1ae0ec730c375a70cf6690db89f11e2203cca57d
Jim Albert <albertcb1@gmail.com>
2019-04-27 18:08:19 -0400

    revise code

--- Statcast_Batter.R
+++ Statcast_Batter.R
@@ -1,23 +1,20 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE,
-                      warning = FALSE, message = FALSE)
+# Exploring Statcast Home Run Data
 
-
-## ------------------------------------------------------------------------
+## install some packages
 library(dplyr)
 library(ggplot2)
 library(readr)
 
 
-## ------------------------------------------------------------------------
+## read in a R function to add the zone to a graph
 source("http://www-math.bgsu.edu/~albert/ACTION/zone.R")
 
 
-## ------------------------------------------------------------------------
+## reads in all home run data from the 2017 season
 hr <- read_csv("http://bit.ly/homerun2017")
 
 
-## ------------------------------------------------------------------------
+## explore Mike Trout's first home run that season
 hr %>% filter(player_name == "Mike Trout") %>% 
   arrange(game_date) %>% 
   head(1) %>% 
@@ -26,7 +23,7 @@ hr %>% filter(player_name == "Mike Trout") %>%
          home_team, away_team)
 
 
-## ------------------------------------------------------------------------
+## plot locations of all home runs hit against curve balls
 ggplot() +
   add_zone(Color = "red") +
   geom_point(data = filter(hr, pitch_type == "CU"),
@@ -34,14 +31,14 @@ ggplot() +
   facet_wrap(~ stand)
 
 
-## ------------------------------------------------------------------------
+## plots the launch angle and launch speed for all hr
 ggplot(data = hr,
              aes(launch_angle, launch_speed)) +
   geom_point(alpha = 0.2) +
   geom_smooth(color = "red")
 
 
-## ------------------------------------------------------------------------
+## home run leaders in 2017?
 hr %>% 
   group_by(player_name) %>% 
   summarize(HR = n()) %>% 
@@ -50,33 +47,32 @@ hr %>%
   head(10)
 
 
-## ------------------------------------------------------------------------
+## who had the hardest hit home run in 2017?
 hr %>% 
   arrange(desc(launch_speed)) %>% 
   head(1) %>% 
   select(player_name, game_date, launch_speed)
 
 
-## ------------------------------------------------------------------------
+## who had the longest hit home run in 2017?
 hr %>% 
   arrange(desc(hit_distance_sc)) %>% 
   head(1) %>% 
   select(player_name, game_date, hit_distance_sc)
 
 
-## ------------------------------------------------------------------------
+## who had the softest hit home run in 2017?
 hr %>% 
   arrange(desc(launch_speed)) %>% 
   tail(1) %>% 
   select(player_name, game_date, launch_speed)
 
 
-## ------------------------------------------------------------------------
+## introduce a count variable
 hr %>% 
  mutate(Count = paste(balls, strikes, sep="-")) -> hr
 
-
-## ------------------------------------------------------------------------
+## home runs hit against different counts
 hr %>% 
  group_by(Count) %>% 
  summarize(N = n()) %>% 
@@ -84,13 +80,13 @@ hr %>%
  geom_col()
 
 
-## ------------------------------------------------------------------------
+## distances of home runs?
 ggplot(hr, aes(hit_distance_sc)) +
   geom_histogram(color = "white", fill = "orange") +
   xlim(300, 500)
 
 
-## ------------------------------------------------------------------------
+## launch speeds of home runs?
 hr %>% 
  group_by(Count) %>% 
  summarize(LS = mean(launch_speed, na.rm = TRUE)) %>% 
@@ -98,7 +94,7 @@ hr %>%
  geom_point()
 
 
-## ------------------------------------------------------------------------
+## mean launch speed of home runs on each count?
 hr %>% 
  group_by(Count) %>% 
  summarize(LS = mean(hit_distance_sc, na.rm = TRUE)) %>% 
@@ -106,7 +102,7 @@ hr %>%
  geom_point()
 
 
-## ------------------------------------------------------------------------
+## spray angle of home runs, how does it vary by batter side?
 hr %>% mutate(
   loc_x = hc_x - 125.42,
   loc_y = 198.27 - hc_y,
--- Statcast_Pitcher.R
+++ Statcast_Pitcher.R
@@ -1,23 +1,19 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE,
-                      warning = FALSE, message = FALSE)
 
-
-## ------------------------------------------------------------------------
+## read in three packages
 library(dplyr)
 library(ggplot2)
 library(readr)
 
 
-## ------------------------------------------------------------------------
+## read in a R function to add the zone to a graph
 source("http://www-math.bgsu.edu/~albert/ACTION/zone.R")
 
 
-## ------------------------------------------------------------------------
+## read in Tribe starter data for 2018 season
 tribe <- read_csv("http://bit.ly/tribepitching")
 
 
-## ------------------------------------------------------------------------
+## remove some rare events and define type variable
 tribe %>% 
   filter((description %in% 
           c("hit_by_pitch", "pitchout")) == FALSE) %>% 
@@ -26,11 +22,10 @@ tribe %>%
           "called_strike"), "Called", "Swing")) -> tribe
 
 
-## ------------------------------------------------------------------------
+## only look at data for Corey Kluber
 ck <- filter(tribe, player_name == "Corey Kluber")
 
-
-## ------------------------------------------------------------------------
+# what pitches does Kluber throw?
 ck %>% 
   group_by(pitch_type) %>% 
   summarize(N = n()) %>% 
@@ -38,24 +33,24 @@ ck %>%
   geom_col()
 
 
-## ------------------------------------------------------------------------
+## How do these pitches compare with respect to speed?
 ck %>% 
   ggplot(aes(pitch_type, release_speed)) +
   geom_boxplot()
 
 
-## ------------------------------------------------------------------------
+## How do these pitches compare with respect to movement?
 ck %>% 
   ggplot(aes(pfx_x, pfx_z, color = pitch_type)) +
   geom_point()
 
 
-## ------------------------------------------------------------------------
+## Let's focus on Kluber's curveballs and cutters
 twopitches <- filter(ck,
                      pitch_type %in% c("CU", "FC"))
 
 
-## ------------------------------------------------------------------------
+## Where does he throw these two pitches?
 twopitches %>% 
   ggplot(aes(plate_x, plate_z)) +
   geom_point() +
@@ -63,40 +58,40 @@ twopitches %>%
   add_zone()
 
 
-## ------------------------------------------------------------------------
+## Locations of called pitches?
 ck %>% filter(Type == "Called") %>% 
   ggplot(aes(plate_x, plate_z, color = description)) +
   geom_point() +
   add_zone()
 
 
-## ------------------------------------------------------------------------
+## Location and outcome of swung pitches?
 ck %>% filter(Type == "Swing") %>% 
   ggplot(aes(plate_x, plate_z, color = description)) +
   geom_point() +
   add_zone()
 
 
-## ------------------------------------------------------------------------
+## Location and outcome of swung curve balls?
 ck %>% filter(Type == "Swing", pitch_type == "CU") %>% 
   ggplot(aes(plate_x, plate_z, color = description)) +
   geom_point() +
   add_zone()
 
 
-## ------------------------------------------------------------------------
+### Location and outcome of swung cutters?
 ck %>% filter(Type == "Swing", pitch_type == "FC") %>% 
   ggplot(aes(plate_x, plate_z, color = description)) +
   geom_point() +
   add_zone()
 
 
-## ------------------------------------------------------------------------
+## Data for Kluber and Baumer
 kluber_bauer <- filter(tribe, player_name %in%
    c("Corey Kluber", "Trevor Bauer"))
 
 
-## ------------------------------------------------------------------------
+## Compare outcomes of swung changeups
 kluber_bauer %>% 
   filter(Type == "Swing", pitch_type == "CH") %>% 
   ggplot(aes(plate_x, plate_z, color = description)) +
@@ -105,14 +100,14 @@ kluber_bauer %>%
   facet_grid(~ player_name)
 
 
-## ------------------------------------------------------------------------
+# Define miss variable
 kluber_bauer %>% 
   mutate(Miss = ifelse(description %in%
   c("swinging_strike", "swinging_strike_blocked"),
   TRUE, FALSE)) -> kluber_bauer
 
 
-## ------------------------------------------------------------------------
+# which pitcher is better in getting misses?
 kluber_bauer %>% 
   filter(Type == "Swing") %>% 
   group_by(player_name, Miss) %>% 
